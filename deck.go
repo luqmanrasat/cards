58bd5c0c1ae72483f4446902116bf4cdec9cab0f
Luqman Rasat <luqmanrasat@goodpeople.my>
2023-10-25 12:25:20 +0800

    use emoji for suit

--- deck.go
+++ deck.go
@@ -5,7 +5,7 @@ import "fmt"
 type deck []string
 
 func newDeck() deck {
-  cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+  cardSuites := []string{"♠️", "♥️", "♣️", "♦️"}
   cardValues := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 
   cards := deck{}
