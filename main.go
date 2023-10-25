6aa4d28ee216e18d5055471c8e6fed01111f5d44
Luqman Rasat <luqmanrasat@hotmail.com>
2023-10-26 04:24:59 +0800

    add load fn

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 func main() {
-  cards := newDeck()
+  // cards := newDeck()
+  cards := loadDeck("remaining_cards.sav")
   hand, remainingCards := deal(cards, 7)
 
   hand.print()
-  remainingCards.saveToFile("remaining_cards")
+  remainingCards.saveToFile("remaining_cards.sav")
 }
