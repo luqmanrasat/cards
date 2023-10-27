3d93cd4026b7c65b06e4efc4b5da88b8bfdcd317
Luqman Rasat <luqmanrasat@hotmail.com>
2023-10-28 04:09:02 +0800

    add shuffle func

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 func main() {
-  // cards := newDeck()
-  cards := loadDeck("remaining_cards.sav")
-  hand, remainingCards := deal(cards, 7)
+  cards := newDeck()
+  // cards := loadDeck("remaining_cards.sav")
+  cards.shuffle()
+  cards.print()
+  // hand, remainingCards := deal(cards, 7)
 
-  hand.print()
-  remainingCards.saveToFile("remaining_cards.sav")
+  // hand.print()
+  // remainingCards.saveToFile("remaining_cards.sav")
 }
