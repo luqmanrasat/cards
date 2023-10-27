bbf771ab640dd226fa23c829f62d4a2f976cdb44
Luqman Rasat <luqmanrasat@goodpeople.my>
2023-10-28 04:43:36 +0800

    add more test for newDeck

--- deck_test.go
+++ deck_test.go
@@ -8,5 +8,12 @@ func TestNewDeck(t *testing.T) {
   if len(d) != 52 {
     t.Errorf("Expected deck length of 52, but got %v", len(d))
   }
-}
 
+  if d[0] != "1 of ♠️" {
+    t.Errorf("Expected deck starts with 1 of ♠️, but got %v", d[0])
+  }
+
+  if d[len(d)-1] != "K of ♦️" {
+    t.Errorf("Expected deck ends with K of ♦️, but got %v", d[len(d)-1])
+  }
+}
